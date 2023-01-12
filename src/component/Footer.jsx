2381284d25d27608b8b0a4b48cf619e6484050ae
putriulhaq <putriulhaq0609@gmail.com>
2023-01-12 16:51:11 +0700

    change ui

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,15 +4,12 @@ import '../style/component/footer.css'
 
 const CompFoot = () => {
     return (
-        <div>
 
-            <div className='text-center p-3' >
-                <span className='text'>Copyright ©2023 ChitChat</span>
-                <a className='text-white' href='https://mdbootstrap.com/' >
-                </a>
-            </div>
+        <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+            </span>
+        </footer>
 
-        </div >
     )
 }
 export default CompFoot;
--- src/pages/Motivate.jsx
+++ src/pages/Motivate.jsx
@@ -37,16 +37,34 @@ const Motivate = () => {
     return (
         <>
             <Navbar />
-            <div className='banner-area two'>
-                <div className="d-table-cell">
-                    <Container fluid>
-                        <div className='bann-content'>
-                            <h1 className="title-banner">Express your feelings, I'll give you motivation.</h1>
+            <section class="text-gray-800 background-radial-gradient ">
+                <div class="bg-[#11393C] px-6 py-12 md:px-12 lg:text-center h-screen mt-12">
+                    <div class="container mx-auto">
+                        <div class="grid lg:grid-cols-1  flex items-center">
+                            <div class="lg:mt-36">
+                                <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12 text-[#E8E9E4]">
+                                    Are you ready <br /><span>for an adventure?</span>
+                                </h1>
+                            </div>
+                            <div>
+                                <textarea id="message" rows="4" class="block p-2.5 w-full text-3xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+                                <textarea id="message" rows="4" class="mt-5 block p-2.5 w-full text-3xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                            </div>
+                            <div>
+                            <button type="button" class="mt-3 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Blue</button>
+                            <button type="button" class="mt-3 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Blue</button>
+                            </div>
                         </div>
-                    </Container>
+                    </div>
                 </div>
-            </div>
-            <Container>
+            </section>
+            {/* <section class="h-8 md:h-full">
+                <div class="bg-[#11393C] px-6 py-12 md:px-12 lg:text-left h-128 mt-12">
+                  
+                </div>
+            </section> */}
+            {/* <Container>
                 <h1></h1>
                 <form onSubmit={handleSubmit}>
                     <textarea className="input-translate"
@@ -60,8 +78,8 @@ const Motivate = () => {
                     </div>
                 </form>
                 <div className="answer">{response}</div>
-            </Container>
-            <Footer />
+            </Container> */}
+            {/* <Footer /> */}
         </>
     )
 }
